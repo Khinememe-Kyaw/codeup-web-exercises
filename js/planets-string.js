fd64ebe8fea7e5021146006b089458d652e020f1
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-04-24 16:28:07 -0500

    Added Planet-string Manipulations and join-split Syntax

---
+++ js/planets-string.js
@@ -0,0 +1,39 @@
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray;
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+    planetsArray = planetsString.split("|");
+    console.log(planetsArray);
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     */
+    // planetsString = planetsString.replaceAll("|", " <br> ");
+    // console.log(planetsString);
+    var brPlanets = planetsArray.join("<br>");
+    console.log(brPlanets);
+    /** BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
+
+    var listPlanets = "<ul>" + "<li>" + planetsArray.join("</li><li>") + "</li>" + "</ul>";
+    console.log(listPlanets);
+
+    // planetsString = planetsString.replaceAll("<br>", " </li><li> ");
+    // var listPlanets = "<ul>" + "<li>" + planetsString + "</li>" + "</ul>";
+    // console.log(listPlanets);
+
+
+})();
