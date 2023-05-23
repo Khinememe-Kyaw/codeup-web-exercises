c27b2da19685b8a446be48a74108e8507aa3e5db
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-05-23 12:32:00 -0500

    Added Selectors and tested out how calling id, class, element and multiple selectors can take precedence over another

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,6 +1,36 @@
-window.onload = function() {
-    alert( 'The page has finished loading!' );
-}
-$(function() {
-    alert( 'The DOM has finished loading!' );
-});
+"use strict";
+
+// window.onload = function() {
+//     alert( 'The page has finished loading!' );
+// }
+// $(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+// Id Selectors
+
+// $(document).ready(function() {
+//     // Select element by id and alert its contents
+//     var firstDivContent = $('#job-engineer').text();
+//     alert(firstDivContent);
+//
+//     // Select a different id and alert its contents
+//     var secondDivContent = $('#job-engineer').text();
+//     alert(secondDivContent);
+// });
+
+// // Class Selectors
+// $('.codeup').css('border', '1px solid #ff0000');
+
+// Element Selectors
+$('li').css('font-size','20px');
+$('.element-problem').css('background', 'blue');
+var elementheading=$('h1').text();
+alert(elementheading);
+
+
+//
+// //All selector
+// $('*').css('border', '3px solid purple');
+
+
