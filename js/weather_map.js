a4aec517d1c37b837eebc54247f7e483ac459ca1
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-05-31 16:21:22 -0500

    Included 5 days of weather info on the website

--- js/weather_map.js
+++ js/weather_map.js
@@ -7,15 +7,29 @@
 })
     .done(function(data) {
     console.log('5 day forecast', data);
-    let todayDate= new Date(data.list[0].dt*1000);
-    let days =[ 'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday', 'Saturday'];
-    let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    var currentDay = days[todayDate.getDay()];
-    var currentMonth = months[todayDate.getMonth()];
-    let weatherHTML = `<section><h1>${currentDay}, ${currentMonth} ${todayDate.getDate()}</h1>
-                                <p>${data.list[0].main.temp_max} / ${data.list[0].main.temp_min}</p></section>`;
+        let weatherHTML = '';
+        let days =[ 'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday', 'Saturday'];
+        let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+        for (let i = 0; i < 5; i++) {
+            let dayIndex = i * 8;
+            let currentData = data.list[dayIndex];
+            let todayDate = new Date(currentData.dt * 1000);
+            var currentDay = days[todayDate.getDay()];
+            var currentMonth = months[todayDate.getMonth()];
+
+        weatherHTML+= `<section>
+            <div>${currentDay}, ${currentMonth} ${todayDate.getDate()}</div>
+            <div>${currentData.main.temp_max} / ${currentData.main.temp_min}</div>
+            <div>Description: ${currentData.weather[0].description}</div>
+            <div>Humidity: ${currentData.main.humidity}</div>
+            <div>Wind: ${currentData.wind.gust}</div>
+            <div>Pressure: ${currentData.main.pressure}</div>
+        </section>`;
+        }
     $("#weather-data").html(weatherHTML);
 });
+
 
 let accessToken = mapboxgl.accessToken = accessKey;
 const weatherMap = new mapboxgl.Map({
@@ -25,5 +39,3 @@ const weatherMap = new mapboxgl.Map({
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
-
-// ${data.list[0].main.temp_max}
