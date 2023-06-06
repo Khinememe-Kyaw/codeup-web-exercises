7321c858473d11da3923640754cb69a29efca292
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-06 15:43:29 -0500

    Used Object Destructuring

--- js/es6.js
+++ js/es6.js
@@ -48,22 +48,26 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-    return emails.push(user.email);
-});
-users.forEach(function(user) {
-    return names.push(user.name);
-});
+// users.forEach(function(user) {
+//     return emails.push(user.email);
+// });
+users.forEach((user) => emails.push(user.email));
+// users.forEach(function(user) {
+//     return names.push(user.name);
+// });
+users.forEach((user) =>names.push(user.name));
+
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
-    const name = user.name;
-    const email = user.email;
-    const languages = user.languages;
+    // const name = user.name;
+    // const email = user.email;
+    // const languages = user.languages;
+    const {name,email,languages} = user;
 
     // TODO: rewrite the assignment below to use template strings
     developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
